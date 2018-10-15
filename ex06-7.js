d710add6b8a06121192146c19180b29a5dc6fb7d
ryo.ogi.0218 <ryo.ogi.0218@gmail.com>
2018-10-14 21:58:46 -0400

    message

--- ex06-7.js
+++ ex06-7.js
@@ -1,10 +1,12 @@
-var who='world';function sayhello(){alert('Hello,'+who+'!');}
-function someone(x){who=x;}
+var who='world';
+function sayhello67(){
+  alert('Hello,'+ who +'!');
+}
+function someone(x){
+  who = x;
+}
 
 
 
 
-<h1>引数の練習</h1>
-<input type="button"value="あいさつ”onclick="sayhello();">
-<input type="button"value="太郎”onclick="sayhello(太郎);">
-<input type="button"value="花子”onclick="sayhello(花子);">
+
